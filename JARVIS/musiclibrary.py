b9cb2d1cd0ea633fed1bea94b3eb951df88f31c5
Ahmar <76834051+Ahmarkalam@users.noreply.github.com>
2025-09-16 13:53:27 -0700

    Update musiclibrary.py

--- JARVIS/musiclibrary.py
+++ JARVIS/musiclibrary.py
@@ -1,7 +1,6 @@
 Music = {
-    "kanye": "https://www.youtube.com/watch?v=ZAz3rnLGthg",
-    "volume": "https://www.youtube.com/watch?v=CXnNv23fSXk"
-    "zeher":  "https://www.youtube.com/watch?v=y12BRDS1CHI&ab_channel=AtifAslam-Topic"
-    
+    "volume": "https://www.youtube.com/watch?v=ZAz3rnLGthg",
+   
 
 }
+
